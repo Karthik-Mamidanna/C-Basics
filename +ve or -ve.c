3b37a0384fa903dacc0ddc42f97a422169161e78
Karthik-Mamidanna <99939875+Karthik-Mamidanna@users.noreply.github.com>
2022-02-20 17:15:54 +0530

    Add files via upload

---
+++ +ve or -ve.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+{
+	int n;
+	printf("Enter a number");
+	scanf("%d",&n);
+	if(n>=0)
+	{
+		printf("The given number is positive");
+		
+	}
+	else{
+		printf("The given number is negative");
+	}
+
+}
